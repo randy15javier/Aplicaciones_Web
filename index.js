5159cec39526da1ff699748cfb2016d9a0761df2
Randy <randyjavierh02@gmail.com>
2020-03-05 15:37:02 -0600

    Modificacion del saludo

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=> {
-    res.send({Hola: 'Mundo'});
+    res.send({Hola: 'Mundo  Cruel'});
 });
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
+// https://glacial-eyrie-07817.herokuapp.com/ 
+//  https://git.heroku.com/glacial-eyrie-07817.git
